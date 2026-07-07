a6948e86e6bf53b9d2e684a026037bf3aabfe7ff
suryakrishnask7 <suryakrishnask21@gmail.com>
2026-07-07 11:23:04 +0530

    Add and Subtract Added

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Cloud Lab!");
-        System.out.println("User Name Change Verification!!");
+        System.out.println("This is a simple calculator program.");
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        int difference = calculator.subtract(10, 4);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        
     }
 }
