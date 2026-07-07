53ae23731804447a9058dcbe11385dd3b90426f3
suryakrishnask7 <suryakrishnask21@gmail.com>
2026-07-07 10:42:59 +0530

    Added .gitignore for the class files

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Cloud Lab!");
+        System.out.println("User Name Change Verification!");
     }
 }
