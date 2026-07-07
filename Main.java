fa810a79fa3fb6ab1e0b4daa0a938d82cf6d3104
25mx121 <25mx121@MCA.PSG>
2026-07-07 11:43:43 +0530

    Everything is done

--- Main.java
+++ Main.java
@@ -2,16 +2,17 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
-        System.out.println("Multiplication = " + calc.multiply(20, 10));
-        System.out.println("Division = " + calc.divide(20, 10));
+
         System.out.println("This is a simple calculator program.");
       
 
-        int sum = calculator.add(5, 3);
-        int difference = calculator.subtract(10, 4);
+        int sum = calc.add(5, 3);
+        int difference = calc.subtract(10, 4);
 
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
+        System.out.println("Multiplication = " + calc.multiply(20, 10));
+        System.out.println("Division = " + calc.divide(20, 10));
         
     }
 }
